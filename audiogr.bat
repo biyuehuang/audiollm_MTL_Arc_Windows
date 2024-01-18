581732ae6f1110a2c512744839a025adf27e97c4
biyuehuang <31006098+biyuehuang@users.noreply.github.com>
2024-01-18 16:24:58 +0800

    Update audiogr.bat

--- audiogr.bat
+++ audiogr.bat
@@ -2,4 +2,4 @@ set SYCL_ENABLE_DEFAULT_CONTEXTS=1
 set SYCL_CACHE_PERSISTENT=1
 call ".\oneAPI\compiler\2023.2.0\env\vars.bat"
 call ".\oneAPI\mkl\2023.2.0\env\vars.bat"
-audio\python.exe gpu_LLM_mtl_KOL.py
+audio\python.exe gpu_LLM_mtl_1205.py
